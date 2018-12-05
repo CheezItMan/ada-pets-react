7880dcd68a500bd745d7d1c595b13444dd1282de
CheezItMan <chris@adadevelopersacademy.org>
2018-12-04 16:33:55 -0800

    Updates

---
+++ precache-manifest.279d56a45873c8e394c6b1034f364939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57e902c194921ac457e3",
+    "url": "/ada-pets-react/static/css/main.878bdbb8.chunk.css"
+  },
+  {
+    "revision": "57e902c194921ac457e3",
+    "url": "/ada-pets-react/static/js/main.57e902c1.chunk.js"
+  },
+  {
+    "revision": "4364dcb78e8352f4b749",
+    "url": "/ada-pets-react/static/css/1.f5e945b1.chunk.css"
+  },
+  {
+    "revision": "4364dcb78e8352f4b749",
+    "url": "/ada-pets-react/static/js/1.4364dcb7.chunk.js"
+  },
+  {
+    "revision": "618cb82b40a6ad0449bc",
+    "url": "/ada-pets-react/static/js/runtime~main.618cb82b.js"
+  },
+  {
+    "revision": "38b447ea044ac86d0f1faa888b46ba7e",
+    "url": "/ada-pets-react/index.html"
+  }
+];
